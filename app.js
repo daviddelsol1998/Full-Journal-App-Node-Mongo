566596de63e009423883f111ed9f801fd1f29941
t-houssian <tylerhoussian@gmail.com>
2022-02-05 22:02:32 -0700

    Added part of User

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Entry = require("./models/entries");
 const entryRouter = require("./routes/entries");
 const methodOverride = require("method-override");
 const path = require('path');
 const app = express();
+
+const User = require('./models/user')
+const Entry = require("./models/entries");
+const authRoutes = require('./routes/auth');
+app.use(authRoutes);
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
 
 const MONGODB_URL = "mongodb+srv://journal-admin:CSE341@cluster0.8ktbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
@@ -19,9 +41,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"));
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.get("/", async (req, res) => {
   const entries = await Entry.find().sort({ createdAt: "desc" });
-  res.render("entries/index", { entries: entries });
+  res.render("entries/index", {
+     entries: entries,
+     isAuthenticated: req.session.isLoggedIn
+    });
 });
 
 app.use("/entries", entryRouter);
