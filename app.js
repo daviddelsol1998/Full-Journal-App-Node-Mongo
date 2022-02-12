edadafdf20f5b7c9b2c279d23ca76cfbef237350
t-houssian <tylerhoussian@gmail.com>
2022-02-11 18:55:30 -0700

    Added a theme

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(methodOverride("_method"));
 
 app.use(
