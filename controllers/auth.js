4c613d326d914a0664313e3ac766bf6790502ebf
t-houssian <tylerhoussian@gmail.com>
2022-02-11 18:57:14 -0700

    Also changed url of reset email

--- controllers/auth.js
+++ controllers/auth.js
@@ -214,7 +214,7 @@ exports.postReset = (req, res, next) => {
           subject: 'Password reset',
           html: `
             <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+            <p>Click this <a href="https://journal-cse341.herokuapp.com/reset/${token}">link</a> to set a new password.</p>
           `
         });
       })
